930289e66037be57b711ce5e4d62fedcbe5a1ef1
DillenGrant <dillen.grant72@gmail.com>
2024-02-03 13:17:30 -0700

    mongo connect

--- server/models/character.js
+++ server/models/character.js
@@ -1,3 +1,13 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/', {
+ useNewUrlParser: true,
+ useUnifiedTopology: true,
+ useCreateIndex: true,
+ useFindAndModify: false
+}).then(() => console.log('MongoDB connected'))
+ .catch((err) => console.log(err));
+
 db.createCollection("Character", {
     validator: {
       $jsonSchema: {
