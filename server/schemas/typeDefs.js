0b672775467f4a7a88e87c034e31e0cac2df9ddd
Krich2022 <117894211+Krich2022@users.noreply.github.com>
2024-02-05 20:43:42 -0700

    finished character in typedefs and fixed user.js bug

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -7,13 +7,35 @@ type User {
     sheets: [Character]!
 }
 
+type Character {
+    _id: ID
+    player: String
+    name: String
+    class: String
+    sub_class: String
+    level: Int
+    strength: Int
+    dexterity: Int
+    constitution: Int
+    intelligence: Int
+    wisdom: Int
+    charisma: Int
+    armor_class: Int
+    created_at: String
+    initiative: Int
+    speed: Int
+    hit_points: Int
+    perception: Int
+    hit_dice: Int
+}
+
 type Auth {
     token: ID!
-    user:User
+    user: User
 }
 
 type Query {
-    users:[User]
+    users: [User]
     user(username: String!): User
     characters(username: String): [Character]
     Character(characterId: ID!): Character
@@ -21,8 +43,10 @@ type Query {
 
 type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
-    login(email: String!, password:String!): Auth
-
+    login(email: String!, password: String!): Auth
+    addCharacter(player: String!, name: String!, class: String!, sub_class: String!, level: Int!, strength: Int!, dexterity: Int!, constitution: Int!, intelligence: Int!, wisdom: Int!, charisma: Int!, armor_class: Int!, initiative: Int!, speed: Int!, hit_points: Int!, perception: Int!, hit_dice: Int!): Character
+    updateCharacter(_id: ID!, player: String, name: String, class: String, sub_class: String, level: Int, strength: Int, dexterity: Int, constitution: Int, intelligence: Int, wisdom: Int, charisma: Int, armor_class: Int, initiative: Int, speed: Int, hit_points: Int, perception: Int, hit_dice: Int): Character
+    deleteCharacter(_id: ID!): Boolean
 }
 `;
 
