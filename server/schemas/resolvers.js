4117b5d7207474e4b025f4c767c0ca907527390b
Krich2022 <117894211+Krich2022@users.noreply.github.com>
2024-02-05 20:54:29 -0700

    finished the resolvers and updated the auth.js file

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,5 @@
 const { User, Character } = require("../models");
+const { signToken, AuthenticationError } = require("../utils/auth");
 
 const resolvers = {
   Query: {
@@ -14,6 +15,43 @@ const resolvers = {
     },
     character: async (parent, { characterId }) => {
       return Character.findOne({ _id: characterId });
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw AuthenticationError;
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw AuthenticationError;
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    addCharacter: async (parent, args, context) => {
+      const character = await Character.create(args);
+      return character;
+    },
+    updateCharacter: async (parent, { _id, ...args }, context) => {
+      const character = await Character.findByIdAndUpdate(_id, args, {
+        new: true,
+      });
+      return character;
+    },
+    deleteCharacter: async (parent, { _id }, context) => {
+      const character = await Character.findByIdAndDelete(_id);
+      return !!character;
     },
   },
 };
