90ae6a89244cf7977c26af45e37ab8c92f946a82
Stretch Spelman <codingspelman@gmail.com>
2024-02-07 19:23:54 -0700

    added most of the details of the character that we need in edit and details.jsx form

--- client/src/pages/CharacterDetails.jsx
+++ client/src/pages/CharacterDetails.jsx
@@ -23,6 +23,84 @@ const CharacterDetails = () => {
         {/* Render spells details */}
       </AccordionSection>
       {/* Add more sections as needed */}
+      <AccordionSection title="Name">
+        {/* Render spells details */}
+      </AccordionSection>
+      <AccordionSection title="SubClass">
+        {/* Render spells details */}
+      </AccordionSection>
+      <AccordionSection title="Background">
+        {/* Render spells details */}
+      </AccordionSection>
+      <AccordionSection title="Alignment">
+        {/* Render spells details */}
+      </AccordionSection>
+      <AccordionSection title="Level">
+        {/* Render spells details */}
+      </AccordionSection>
+      <AccordionSection title="Xp">
+        {/* Render spells details */}
+      </AccordionSection>
+      <AccordionSection title="Strength">
+        {/* Render spells details */}
+      </AccordionSection>
+      <AccordionSection title="Dexterity">
+        {/* Render spells details */}
+      </AccordionSection>
+      <AccordionSection title="Constitution">
+        {/* Render spells details */}
+      </AccordionSection>
+      <AccordionSection title="Inteligence">
+        {/* Render spells details */}
+      </AccordionSection>
+      <AccordionSection title="Wisdom">
+        {/* Render spells details */}
+      </AccordionSection>
+      <AccordionSection title="Charisma">
+        {/* Render spells details */}
+      </AccordionSection>
+      <AccordionSection title="Proficiency">
+        {/* Render spells details */}
+      </AccordionSection>
+      <AccordionSection title="Saving_Throws">
+        {/* Render spells details */}
+      </AccordionSection>
+      <AccordionSection title="Ac">
+        {/* Render spells details */}
+      </AccordionSection>
+      <AccordionSection title="Speed">
+        {/* Render spells details */}
+      </AccordionSection>
+      <AccordionSection title="Hp">
+        {/* Render spells details */}
+      </AccordionSection>
+      <AccordionSection title="Temp_Hp">
+        {/* Render spells details */}
+      </AccordionSection>
+      <AccordionSection title="Traits">
+        {/* Render spells details */}
+      </AccordionSection>
+      <AccordionSection title="Ideals">
+        {/* Render spells details */}
+      </AccordionSection>
+      <AccordionSection title="Bonds">
+        {/* Render spells details */}
+      </AccordionSection>
+      <AccordionSection title="Flaws">
+        {/* Render spells details */}
+      </AccordionSection>
+      <AccordionSection title="Languages">
+        {/* Render spells details */}
+      </AccordionSection>
+      <AccordionSection title="Money">
+        {/* Render spells details */}
+      </AccordionSection>
+      <AccordionSection title="Abilities">
+        {/* Render spells details */}
+      </AccordionSection>
+      
+
+
     </div>
   );
 };
