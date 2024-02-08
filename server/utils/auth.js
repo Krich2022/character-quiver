3939de3eef49430485cac6c8cf844fd55292e1af
Krich2022 <117894211+Krich2022@users.noreply.github.com>
2024-02-07 18:36:29 -0700

    updated auth on client and server

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -10,8 +10,29 @@ module.exports = {
       code: "UNAUTHENTICATED",
     },
   }),
-  signToken: function ({ email, username, _id }) {
-    const payload = { email, username, _id };
+  authMiddleware: function ({ req }) {
+    let token = req.body.token || req.query.token || req.headers.authorization;
+
+    if (req.headers.authorization) {
+      token = token.split(" ").pop().trim();
+    }
+
+    if (!token) {
+      return req;
+    }
+
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log("Invalid token");
+    }
+
+    return req;
+  },
+  signToken: function ({ name, email, _id }) {
+    const payload = { name, email, _id };
+
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
