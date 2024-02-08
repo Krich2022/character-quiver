76dc07f83010e8448cb82b404db3160e479d852f
LonliLucas <lukej.gilless@gmail.com>
2024-02-07 21:15:03 -0700

    added rendering to main.jsx and removed antd

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.jsx';
+import './index.css';
+import Login from './pages/login.jsx';
+import Signup from './pages/Signup.jsx';
+
+const router = createBrowserRouter([
+  { 
+    path: '/', 
+    element: <App />, 
+    children: [{
+      index: true,
+      element: <Login />,
+    },
+    {
+      path: '/signup',
+      element: <Signup />
+    }] 
+  }
+  ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
