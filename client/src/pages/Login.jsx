17378b0bcfcc57e98fc4594a749dc11d831d35a8
LonliLucas <lukej.gilless@gmail.com>
2024-02-08 19:12:40 -0700

    Changed css framework and component library

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,17 +1,18 @@
-import React from 'react';
+import Form from 'react-bootstrap/Form';
 
 function Login() {
     return (
-        <div direction="vertical" className="container-background p-10 rounded-2xl">
-            <h2 className="text-xl">Login to your account</h2>
+        <>
+            <div direction="vertical" className="container-background p-10 rounded-2xl">
+                <h2 className="text-xl">Login to your account</h2>
 
-            <form>
-                <div>
-                </div>
-            </form>
-            <p className="text-lg">Don't have an account?</p>
-            
-        </div>
+                <form>
+                    <Form.Control type="password" id="inputPassword" placeholder="password" className=''></Form.Control>
+                </form>
+                <p className="text-lg">Don't have an account?</p>
+                
+            </div>
+        </>
     )
 }
 
