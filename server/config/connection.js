2c4ac97ed1278f14e3c8f61087588ed2e43ad281
Krich2022 <117894211+Krich2022@users.noreply.github.com>
2024-02-11 21:10:25 -0700

    hopefull db fix

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,26 @@
-const mongoose = require("mongoose");
-
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/character-quiver"
-);
-
-module.exports = mongoose.connection;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://project:<password>@cluster0.xxktq3d.mongodb.net/?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
