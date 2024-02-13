c4506db9b4ba4b1e8a65f8428ff83ef850f7fc8f
DillenGrant <dillen.grant72@gmail.com>
2024-02-12 20:12:08 -0700

    landing page style

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -2,24 +2,25 @@ import React from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
+export default Landing;
 const Landing = () => {
   return (
-    <Container fluid className="landing">
+    <Container fluid className="landing landing-background container-background">
       <header className="landing-header text-center py-5">
         <h1>Welcome to Character Quiver</h1>
         <p>Start your journey with our easy-to-use character builder.</p>
+        <div className="custom-image-container">
+          <img src="/assets/cqDrawing.jpg" alt="Large Custom Picture" className="custom-image" />
+          <div className="login-button-wrapper">
+            <Link to="/login" className="login-button-link">
+              <Button variant="primary" className="button-background">Login</Button>
+            </Link>
+          </div>
+        </div>
       </header>
-      <div className="custom-image-container">
-        <img src="../src/assets/BONBDYN1ER6A3KJGB92QYHS494LY6CD7_0 (2).jpg" alt="Large Custom Picture" className="custom-image" />
-        <Link to="/login" className="login-button-link">
-          <Button variant="primary">Login</Button>
-        </Link>
-      </div>
       <footer className="landing-footer text-center py-3">
         <p>&copy;   2023 Character Quiver</p>
       </footer>
     </Container>
   );
 };
-
-export default Landing;
