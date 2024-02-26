3485d0837b10c8a2253bd750c4d11f260a68c1b9
Krich2022 <117894211+Krich2022@users.noreply.github.com>
2024-02-25 23:34:37 -0700

    created objects for the selects

--- client/src/components/ui/Transition.jsx
+++ client/src/components/ui/Transition.jsx
@@ -1,26 +1,30 @@
 import { useState } from "react";
+import { Form } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Collapse from "react-bootstrap/Collapse";
-
-export default function CharacterTransition() {
+export default function CharacterTransition(option) {
   const [open, setOpen] = useState(false);
+
   return (
-    <>
+    <div>
       <Button
         onClick={() => setOpen(!open)}
         aria-controls="example-collapse-text"
         aria-expanded={open}>
-        click
+        {option.title}
       </Button>
       <Collapse in={open}>
         <div
           className="container-background  enable-rounded text-color"
           id="example-collapse-text">
-          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
-          terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
-          labore wes anderson cred nesciunt sapiente ea proident.
+          {option.select.map((selectOption, index) => (
+            <Form.Select key={index} aria-label={`Select ${selectOption}`}>
+              <option>Select {selectOption}</option>
+              {/* You can populate the options based on your requirements */}
+            </Form.Select>
+          ))}
         </div>
       </Collapse>
-    </>
+    </div>
   );
 }
--- client/src/pages/testCharacter.jsx
+++ client/src/pages/testCharacter.jsx
@@ -2,6 +2,25 @@ import Transition from "../components/ui/Transition";
 export default function CreateCharacter() {
   var myHeaders = new Headers();
   myHeaders.append("Accept", "application/json");
+  const options = [
+    {
+      title: "Character Details",
+      select: ["race", "class", "background", "alignment"],
+      numberInputs: ["Hit Points", "STR", "DEX", "CON", "WIS", "INT", "CHA"],
+    },
+    {
+      title: "Features & Traits",
+      select: ["race", "class", "background", "alignment"],
+    },
+    {
+      title: "Equipment",
+      select: ["race", "class", "background", "alignment"],
+    },
+    {
+      title: "Character Background",
+      select: ["race", "class", "background", "alignment"],
+    },
+  ];
 
   var requestOptions = {
     method: "GET",
@@ -15,7 +34,9 @@ export default function CreateCharacter() {
     .catch((error) => console.log("error", error));
   return (
     <>
-      <Transition />
+      {options.map((option, index) => (
+        <Transition {...option} />
+      ))}
     </>
   );
 }
